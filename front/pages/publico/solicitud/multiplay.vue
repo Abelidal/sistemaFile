7b51ae76f6fb96793534783bc414875dd7fc1634
Abel Adalid Quispe Iturry <abel.adalid@gmail.com>
2021-07-20 17:56:37 -0400

    Modificacion archivo

--- front/pages/publico/solicitud/multiplay.vue
+++ front/pages/publico/solicitud/multiplay.vue
@@ -8,80 +8,83 @@
                     <form>
                         <v-row no-gutters>
                             <v-col>
-                                <v-select :items="items" v-model="fibraMovil.plan_id" label="Elige tu Plan"></v-select>
+                                <v-select :items="items" v-model="multiplay.plan_id" label="Elige tu Plan"></v-select>
                             </v-col>
                             <v-col>
-                                <v-select :items="items" v-model="fibraMovil.nros_beneficio_id" label="Líneas Beneficiaras"></v-select>
+                                <v-select :items="items" v-model="multiplay.nros_beneficio_id" label="Líneas Beneficiaras"></v-select>
                             </v-col>
                         </v-row>
                         <v-col no-gutters>
                             <v-row>
-                                <v-text-field label="Línea Beneficiaria 1" :rules="lineaBeneficio1" v-model="fibraMovil.lineaBeneficio1" hide-details="auto"></v-text-field>
+                                <v-text-field label="Línea Beneficiaria 1" :rules="lineaBeneficio1" v-model="multiplay.lineaBeneficio1" hide-details="auto"></v-text-field>
                             </v-row>
                             <v-row>
-                                <v-checkbox v-model="fibraMovil.checkboxMb_l1" label="Mb"></v-checkbox>
+                                <v-checkbox v-model="multiplay.checkboxMb_l1" label="Mb"></v-checkbox>
                             </v-row>
                             <v-row>
-                                <v-checkbox v-model="fibraMovil.checkboxWhastapp_l1" label="Whatsapp"></v-checkbox>
+                                <v-checkbox v-model="multiplay.checkboxWhastapp_l1" label="Whatsapp"></v-checkbox>
                             </v-row>
                             <v-row>
-                                <v-checkbox v-model="fibraMovil.checkboxMinutos_l1" label="Minutos"></v-checkbox>
+                                <v-checkbox v-model="multiplay.checkboxMinutos_l1" label="Minutos"></v-checkbox>
                             </v-row>
                             <v-row>
-                                <v-checkbox v-model="fibraMovil.checkboxSms_l1" label="Minutos"></v-checkbox>
+                                <v-checkbox v-model="multiplay.checkboxSms_l1" label="Minutos"></v-checkbox>
                             </v-row>                        
                         </v-col>
                         <v-col no-gutters>
                             <v-col>
-                                <v-text-field label="Línea Beneficiaria 2" :rules="lineaBeneficio2" v-model="fibraMovil.lineaBeneficio2" hide-details="auto"></v-text-field>
+                                <v-text-field label="Línea Beneficiaria 2" :rules="lineaBeneficio2" v-model="multiplay.lineaBeneficio2" hide-details="auto"></v-text-field>
                             </v-col>
                             <v-row>
-                                <v-checkbox v-model="fibraMovil.checkboxMb_l2" label="Mb"></v-checkbox>
+                                <v-checkbox v-model="multiplay.checkboxMb_l2" label="Mb"></v-checkbox>
                             </v-row>
                             <v-row>
-                                <v-checkbox v-model="fibraMovil.checkboxWhastapp_l2" label="Whatsapp"></v-checkbox>
+                                <v-checkbox v-model="multiplay.checkboxWhastapp_l2" label="Whatsapp"></v-checkbox>
                             </v-row>
                             <v-row>
-                                <v-checkbox v-model="fibraMovil.checkboxMinutos_l2" label="Minutos"></v-checkbox>
+                                <v-checkbox v-model="multiplay.checkboxMinutos_l2" label="Minutos"></v-checkbox>
                             </v-row>
                             <v-row>
-                                <v-checkbox v-model="fibraMovil.checkboxSms_l2" label="Minutos"></v-checkbox>
+                                <v-checkbox v-model="multiplay.checkboxSms_l2" label="Minutos"></v-checkbox>
                             </v-row> 
                             <v-row>
                                 <v-btn outlined color="indigo">Nro. Favorito</v-btn>            
                             </v-row>     
                         </v-col>
+                        <v-col>
+                            <v-select :items="items" v-model="multiplay.cantidadTv_id" label="¿Cuántos Televisores requieres?"></v-select>
+                        </v-col>
                         <v-row no-gutters>    
                             <v-col>
-                                <v-select :items="items" v-model="fibraMovil.genero_id" label="Género"></v-select>
+                                <v-select :items="items" v-model="multiplay.genero_id" label="Género"></v-select>
                             </v-col>
                             <v-col>
-                                <v-select :items="items" v-model="fibraMovil.estadoCivil_id" label="Estado Civil"></v-select>
+                                <v-select :items="items" v-model="multiplay.estadoCivil_id" label="Estado Civil"></v-select>
                             </v-col>
                         </v-row>
                         
                         <v-col>
-                            <v-text-field label="Nombre (s)" :rules="nombres" v-model="fibraMovil.nombres" hide-details="auto"></v-text-field>
+                            <v-text-field label="Nombre (s)" :rules="nombres" v-model="multiplay.nombres" hide-details="auto"></v-text-field>
                         </v-col>
                         <v-col>
-                            <v-text-field label="Apellido Paterno" :rules="apellidoPaterno" v-model="fibraMovil.apellidoPaterno" hide-details="auto"></v-text-field>
+                            <v-text-field label="Apellido Paterno" :rules="apellidoPaterno" v-model="multiplay.apellidoPaterno" hide-details="auto"></v-text-field>
                         </v-col>
                         <v-col>
-                            <v-text-field label="Apellido Materno" :rules="apellidoMaterno" v-model="fibraMovil.apellidoMaterno" hide-details="auto"></v-text-field>
+                            <v-text-field label="Apellido Materno" :rules="apellidoMaterno" v-model="multiplay.apellidoMaterno" hide-details="auto"></v-text-field>
                         </v-col>
                         <v-col>
-                            <v-text-field label="Apellido Marital" :rules="apellidoMarital" v-model="fibraMovil.apellidoMarital" hide-details="auto"></v-text-field>
+                            <v-text-field label="Apellido Marital" :rules="apellidoMarital" v-model="multiplay.apellidoMarital" hide-details="auto"></v-text-field>
                         </v-col>
                         <v-row no-gutters>
-                            <v-select :items="items" v-model="fibraMovil.tipoDocumento_id" label="Tipo de Documento"></v-select>        
+                            <v-select :items="items" v-model="multiplay.tipoDocumento_id" label="Tipo de Documento"></v-select>        
                         </v-row>
 
                         <v-row no-gutters>
                             <v-col>
-                                <v-text-field label="Número de Documento" :rules="nroDocumento" v-model="fibraMovil.nroDocumento" hide-details="auto"></v-text-field>
+                                <v-text-field label="Número de Documento" :rules="nroDocumento" v-model="multiplay.nroDocumento" hide-details="auto"></v-text-field>
                             </v-col>
                             <v-col>
-                                <v-select :items="items" v-model="fibraMovil.lugarExpedicion_id" label="Lugar de Expedición"></v-select>
+                                <v-select :items="items" v-model="multiplay.lugarExpedicion_id" label="Lugar de Expedición"></v-select>
                             </v-col>
                         </v-row>
                        
@@ -96,7 +99,7 @@
                             >
                                 <template v-slot:activator="{ on, attrs }">
                                 <v-text-field
-                                    v-model="fibraMovil.date_id"
+                                    v-model="multiplay.date_id"
                                     label="Fecha de Nacimiento"
                                     prepend-icon="mdi-calendar"
                                     readonly
@@ -112,7 +115,7 @@
                         </v-col>
 
                         <v-col>
-                            <v-text-field :items="items" v-model="fibraMovil.correo" label="Correo Electrónico"></v-text-field>
+                            <v-text-field :items="items" v-model="multiplay.correo" label="Correo Electrónico"></v-text-field>
                         </v-col>
                         
                         <div class="pa-2 headline grey lighten-2  text-center">
@@ -120,39 +123,39 @@
                         </div>
                         <v-row>
                             <v-col>
-                                <v-select :items="items" label="Departamento - Ciudad" v-model="fibraMovil.ciudad_id" hide-details="auto"></v-select>
+                                <v-select :items="items" label="Departamento - Ciudad" v-model="multiplay.ciudad_id" hide-details="auto"></v-select>
                             </v-col>
 
                             <v-col>
-                                <v-select :items="items" label="Provincia" v-model="fibraMovil.provincia_id" hide-details="auto"></v-select>
+                                <v-select :items="items" label="Provincia" v-model="multiplay.provincia_id" hide-details="auto"></v-select>
                             </v-col>
                         </v-row>
                         <v-row>
                             <v-col>
-                                <v-text-field :items="items" v-model="fibraMovil.zona" label="Zona / Barrio"></v-text-field>
+                                <v-text-field :items="items" v-model="multiplay.zona" label="Zona / Barrio"></v-text-field>
                             </v-col>
                             <v-col>
-                                <v-text-field :items="items" v-model="fibraMovil.calleAvenida" label="Calle o Avenida"  hide-details="auto"></v-text-field>
+                                <v-text-field :items="items" v-model="multiplay.calleAvenida" label="Calle o Avenida"  hide-details="auto"></v-text-field>
                             </v-col>
                         </v-row>
                         <v-row>
                             <v-col>
-                                <v-select :items="items" label="Tipo de Vivienda" v-model="fibraMovil.tipoVivienda_id" hide-details="auto"></v-select>
+                                <v-select :items="items" label="Tipo de Vivienda" v-model="multiplay.tipoVivienda_id" hide-details="auto"></v-select>
                             </v-col>
                             <v-col>
-                                <v-text-field :items="items" v-model="fibraMovil.nroVivienda" label="Número de Vivienda"  hide-details="auto"></v-text-field>    
+                                <v-text-field :items="items" v-model="multiplay.nroVivienda" label="Número de Vivienda"  hide-details="auto"></v-text-field>    
                             </v-col>
                         </v-row>                
                         <v-col>
-                                <v-text-field :items="items" v-model="fibraMovil.referencias" label="Referencias de la Dirección" hide-details="auto"></v-text-field>    
+                                <v-text-field :items="items" v-model="multiplay.referencias" label="Referencias de la Dirección" hide-details="auto"></v-text-field>    
                         </v-col>   
                         
                        <v-row>
                             <v-col>
-                                <v-text-field label="Numero de Celular de referencia" :items="items" v-model="fibraMovil.celularRef" hide-details="auto"></v-text-field>
+                                <v-text-field label="Numero de Celular de referencia" :items="items" v-model="multiplay.celularRef" hide-details="auto"></v-text-field>
                             </v-col>
                             <v-col>
-                                <v-text-field label="Numero de Teléfono de referencia" :items="items" v-model="fibraMovil.telefonoRef" hide-details="auto"></v-text-field>
+                                <v-text-field label="Numero de Teléfono de referencia" :items="items" v-model="multiplay.telefonoRef" hide-details="auto"></v-text-field>
                             </v-col>
                         </v-row>
 
@@ -166,7 +169,7 @@
                                 placeholder="Pick an avatar"
                                 prepend-icon="mdi-camera"
                                 label="Envíe la foto de su documento de identidad"
-                                v-model="fibraMovil.fotoCI"
+                                v-model="multiplay.fotoCI"
                             ></v-file-input>
                         </v-col>
                         <v-col>
@@ -176,7 +179,7 @@
                                 placeholder="Pick an avatar"
                                 prepend-icon="mdi-camera"
                                 label="Envíe la foto de Factura se servicio básico"
-                                v-model="fibraMovil.fotoFactura"
+                                v-model="multiplay.fotoFactura"
                             ></v-file-input>
                         </v-col>
                          <v-col>
@@ -187,7 +190,7 @@
                                     placeholder="Presione en el mapa"
                                     prepend-icon="mdi-camera"
                                     label="Seleccione en el mapa su Ubicación"
-                                    v-model="fibraMovil.ubicación"
+                                    v-model="multiplay.ubicación"
                                 ></v-file-input>
                         </v-row>
                             
@@ -201,7 +204,7 @@
                                     placeholder="Pick an avatar"
                                     prepend-icon="mdi-camera"
                                     label="Envíe la foto de su referencia financiera"
-                                    v-model="fibraMovil.referenciaFinanciera"
+                                    v-model="multiplay.referenciaFinanciera"
                                 ></v-file-input>
                             </v-row>
                             
@@ -226,7 +229,7 @@ export default {
     data: () => ({
         date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
         menu2: false,
-        fibraMovil:{},
+        multiplay:{},
         checkbox: true,
         rules: [
             value => !!value || 'Required.',
