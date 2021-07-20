a4eabf37df43634a3f0267a0407a28f5dded7574
Abel Adalid Quispe Iturry <abel.adalid@gmail.com>
2021-07-20 17:28:35 -0400

    Modificaciones para el plan Simple Ilimitado

--- front/pages/publico/solicitud/simpleIlimitado.vue
+++ front/pages/publico/solicitud/simpleIlimitado.vue
@@ -2,36 +2,53 @@
     <div class="pa-2">
         <v-card>
             <div class="pa-2 headline grey lighten-2  text-center">
-                <h5>Simple Ilimitado</h5>
+                <h5>Simple Ilimitado Elección de Planes y Datos Personales</h5>
             </div>
                 <v-container>
                     
                     <form>
                         <v-row no-gutters>
                             <v-col>
-                                <v-select :items="items" v-model="simpleIlimitado.plan_id" label="Plan"></v-select>
+                                <v-select :items="items" v-model="simpleIlimitado.plan_id" label="Elige tu Plan"></v-select>
                             </v-col>
                             <v-col>
                                 <v-btn outlined color="indigo">Nro. Favorito</v-btn>            
                             </v-col>
                         </v-row>
-                        <v-row no-gutters>
+                        <v-row no-gutters>    
                             <v-col>
-                                <v-text-field label="ci" :rules="rules" v-model="simpleIlimitado.ci" hide-details="auto"></v-text-field>
+                                <v-select :items="items" v-model="simpleIlimitado.genero_id" label="Género"></v-select>
                             </v-col>
                             <v-col>
-                                <v-select :items="items" label="Pais"></v-select>
+                                <v-select :items="items" v-model="simpleIlimitado.estadoCivil_id" label="Estado Civil"></v-select>
                             </v-col>
                         </v-row>
+                        
                         <v-col>
-                            <v-text-field label="Nombres" :rules="nombres" hide-details="auto"></v-text-field>
+                            <v-text-field label="Nombre (s)" :rules="nombres" v-model="simpleIlimitado.nombres" hide-details="auto"></v-text-field>
                         </v-col>
                         <v-col>
-                            <v-text-field label="Apellidos" :rules="apellidos" hide-details="auto"></v-text-field>
+                            <v-text-field label="Apellido Paterno" :rules="apellidoPaterno" v-model="simpleIlimitado.apellidoPaterno" hide-details="auto"></v-text-field>
                         </v-col>
                         <v-col>
-                            <v-select :items="items" label="Ciudad"></v-select>
+                            <v-text-field label="Apellido Materno" :rules="apellidoMaterno" v-model="simpleIlimitado.apellidoMaterno" hide-details="auto"></v-text-field>
                         </v-col>
+                        <v-col>
+                            <v-text-field label="Apellido Marital" :rules="apellidoMarital" v-model="simpleIlimitado.apellidoMarital" hide-details="auto"></v-text-field>
+                        </v-col>
+                        <v-row no-gutters>
+                            <v-select :items="items" v-model="simpleIlimitado.tipoDocumento_id" label="Tipo de Documento"></v-select>        
+                        </v-row>
+
+                        <v-row no-gutters>
+                            <v-col>
+                                <v-text-field label="Número de Documento" :rules="nroDocumento" v-model="simpleIlimitado.nroDocumento" hide-details="auto"></v-text-field>
+                            </v-col>
+                            <v-col>
+                                <v-select :items="items" v-model="simpleIlimitado.lugarExpedicion_id" label="Lugar de Expedición"></v-select>
+                            </v-col>
+                        </v-row>
+                       
                         <v-col>
                             <v-menu
                                 v-model="menu2"
@@ -43,8 +60,8 @@
                             >
                                 <template v-slot:activator="{ on, attrs }">
                                 <v-text-field
-                                    v-model="date"
-                                    label="Picker without buttons"
+                                    v-model="simpleIlimitado.date_id"
+                                    label="Fecha de Nacimiento"
                                     prepend-icon="mdi-calendar"
                                     readonly
                                     v-bind="attrs"
@@ -57,23 +74,80 @@
                                 ></v-date-picker>
                             </v-menu>
                         </v-col>
+
                         <v-col>
-                            <v-text-field label="Direccion" :rules="direccion" hide-details="auto"></v-text-field>
+                            <v-text-field :items="items" v-model="simpleIlimitado.correo" label="Correo Electrónico"></v-text-field>
                         </v-col>
+                        
+                        <div class="pa-2 headline grey lighten-2  text-center">
+                            <h6>Dirección</h6>
+                        </div>
+                        <v-row>
+                            <v-col>
+                                <v-select :items="items" label="Departamento - Ciudad" v-model="simpleIlimitado.ciudad_id" hide-details="auto"></v-select>
+                            </v-col>
+
+                            <v-col>
+                                <v-select :items="items" label="Provincia" v-model="simpleIlimitado.provincia_id" hide-details="auto"></v-select>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col>
+                                <v-text-field :items="items" v-model="simpleIlimitado.zona" label="Zona / Barrio"></v-text-field>
+                            </v-col>
+                            <v-col>
+                                <v-text-field :items="items" v-model="simpleIlimitado.calleAvenida" label="Calle o Avenida"  hide-details="auto"></v-text-field>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col>
+                                <v-select :items="items" label="Tipo de Vivienda" v-model="simpleIlimitado.tipoVivienda_id" hide-details="auto"></v-select>
+                            </v-col>
+                            <v-col>
+                                <v-text-field :items="items" v-model="simpleIlimitado.nroVivienda" label="Número de Vivienda"  hide-details="auto"></v-text-field>    
+                            </v-col>
+                        </v-row>                
                         <v-col>
-                            <v-row>
-                                <v-checkbox v-model="checkbox"></v-checkbox>
-                                <v-text-field label="Numero de Celular" :rules="rules" hide-details="auto"></v-text-field>
-                            </v-row>
-                            
+                                <v-text-field :items="items" v-model="simpleIlimitado.referencias" label="Referencias de la Dirección" hide-details="auto"></v-text-field>    
+                        </v-col>   
+                        
+                       <v-row>
+                            <v-col>
+                                <v-text-field label="Numero de Celular de referencia" :items="items" v-model="simpleIlimitado.celularRef" hide-details="auto"></v-text-field>
+                            </v-col>
+                            <v-col>
+                                <v-text-field label="Numero de Teléfono de referencia" :items="items" v-model="simpleIlimitado.telefonoRef" hide-details="auto"></v-text-field>
+                            </v-col>
+                        </v-row>
+
+                        <div class="pa-2 headline grey lighten-2  text-center">
+                            <h6>Elección de nuevo número o migración a Pospago</h6>
+                        </div>
+                        <v-row>
+                            <v-col>
+                                <v-select :items="items" label="Para que Ciudad requiere su Línea" v-model="simpleIlimitado.ciudad_id" hide-details="auto"></v-select>
+                            </v-col>
+
+                            <v-col>
+                                <v-select :items="items" label="Provincia" v-model="simpleIlimitado.provincia_id" hide-details="auto"></v-select>
+                            </v-col>
+                        </v-row>
+                        <v-col>
+                            <v-checkbox v-model="simpleIlimitado.checkboxPospagoIlimitado" label="¿Tiene una línea Entel que pasará a pospago Simple Ilimitado?"></v-checkbox>
+                                
+                            <v-text-field label="Numero de Teléfono que pasa a Pospago" :items="items" v-model="simpleIlimitado.numeroPospago" hide-details="auto"></v-text-field>
                         </v-col>
+                       <div class="pa-2 headline grey lighten-2  text-center">
+                            <h6>Envío de Documentos</h6>
+                        </div>
                         <v-col>
                             <v-file-input
                                 :rules="rules"
                                 accept="image/png, image/jpeg, image/bmp"
                                 placeholder="Pick an avatar"
                                 prepend-icon="mdi-camera"
-                                label="Avatar"
+                                label="Envíe la foto de su documento de identidad"
+                                v-model="simpleIlimitado.fotoCI"
                             ></v-file-input>
                         </v-col>
                         <v-col>
@@ -82,7 +156,8 @@
                                 accept="image/png, image/jpeg, image/bmp"
                                 placeholder="Pick an avatar"
                                 prepend-icon="mdi-camera"
-                                label="Avatar"
+                                label="Envíe la foto de Factura se servicio básico"
+                                v-model="simpleIlimitado.fotoFactura"
                             ></v-file-input>
                         </v-col>
                         <v-col>
@@ -93,7 +168,8 @@
                                     accept="image/png, image/jpeg, image/bmp"
                                     placeholder="Pick an avatar"
                                     prepend-icon="mdi-camera"
-                                    label="Avatar"
+                                    label="Envíe la foto de su referencia financiera"
+                                    v-model="simpleIlimitado.referenciaFinanciera"
                                 ></v-file-input>
                             </v-row>
                             
